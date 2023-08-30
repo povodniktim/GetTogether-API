0159d1e18f21c299360246cb77cbf42685edd564
Tim Povodnik <47430888+povodniktim@users.noreply.github.com>
2023-08-30 22:16:00 +0200

    Added missing basic CRUD operations to UserController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -18,9 +18,73 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<Users>> GetAll()
+        public async Task<ActionResult<Users>> Get()
         {
             return Ok(_context.Users);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Users>> Create([FromBody] Users user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = user.ID }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Users user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (id != user.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Users>> Delete(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(u => u.ID == id);
         }
     }
 }
